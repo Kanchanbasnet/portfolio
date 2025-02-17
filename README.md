9e6f7d2aaaa18d241fc1680255ddbc3dc7530c1f
Kanchan Basnet <kanchan.basnet@frostdigi.ai>
2025-02-17 22:56:41 +0545

    feat: initial project setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Portfolio Website
+
+This is a **monorepo** setup for a personal portfolio website using **PNPM workspaces**. The project consists of:
+
+- A **React** frontend
+- A **Node.js** backend
+- **ESLint, Prettier, and Husky** for code formatting and linting
+- **PNPM workspace** for managing dependencies
+
+---
---
+++ apps/api/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+// import utils from 'utils';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Api running successfully' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
